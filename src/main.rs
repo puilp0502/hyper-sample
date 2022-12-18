e5dbefb1b1e9af88302b65a32c625c8f776b58b8
Frank Yang <puilp0502@gmail.com>
2022-12-18 23:26:22 +0900

    implement hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::Server;
+use hyper::service::{make_service_fn, service_fn};
+use hyper_start::hello_world;
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let make_svc = make_service_fn(|conn| async {
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
+
