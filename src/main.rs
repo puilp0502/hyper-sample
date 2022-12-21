f9cc67d9ffee457624b7f2921d789b87b237aa34
Frank Yang <puilp0502@gmail.com>
2022-12-22 02:32:29 +0900

    feat: add tcp_nodelay

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use std::convert::Infallible;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, TcpListener};
+use std::os::fd::AsRawFd;
 use hyper::Server;
 use hyper::service::{make_service_fn, service_fn};
+use nix::sys::socket::setsockopt;
+use nix::sys::socket::sockopt::TcpNoDelay;
 use hyper_start::hello_world;
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
+    let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
+    let fd = listener.as_raw_fd();
+    let _ = setsockopt(fd, TcpNoDelay, &true).unwrap();
     let make_svc = make_service_fn(|_conn| async {
         Ok::<_, Infallible>(service_fn(hello_world))
     });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::from_tcp(listener).unwrap().serve(make_svc);
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
